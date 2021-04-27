e02b51bd71888f468edc1bea9afa31b492bf39dd
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-04-27 14:47:33 +0700

    Add files via upload

---
+++ WebView2/WebView2/frmWebView.designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmWebView
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmWebView))
+        Me.WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        Me.txtURL = New System.Windows.Forms.TextBox()
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'WebView21
+        '
+        Me.WebView21.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebView21.CreationProperties = Nothing
+        Me.WebView21.DefaultBackgroundColor = System.Drawing.Color.White
+        Me.WebView21.Location = New System.Drawing.Point(4, 43)
+        Me.WebView21.Name = "WebView21"
+        Me.WebView21.Size = New System.Drawing.Size(839, 581)
+        Me.WebView21.TabIndex = 1
+        Me.WebView21.ZoomFactor = 1.0R
+        '
+        'txtURL
+        '
+        Me.txtURL.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtURL.Location = New System.Drawing.Point(4, 12)
+        Me.txtURL.Name = "txtURL"
+        Me.txtURL.Size = New System.Drawing.Size(839, 23)
+        Me.txtURL.TabIndex = 0
+        '
+        'frmWebView
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(844, 628)
+        Me.Controls.Add(Me.txtURL)
+        Me.Controls.Add(Me.WebView21)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmWebView"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "WebView2 - coDe bY: Thongkorn Tubtimkrob"
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
+    Friend WithEvents txtURL As TextBox
+End Class
---
+++ WebView2/WebView2/frmWebView.vb
@@ -0,0 +1,21 @@
+﻿Public Class frmWebView
+    Private Sub frmWebView_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtURL.Text = "https://www.google.co.th"
+    End Sub
+
+    Private Sub txtURL_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtURL.KeyPress
+        '// Press ENTER
+        If Asc(e.KeyChar) = 13 Then
+            '// No beep
+            e.Handled = True
+            Try
+                '// Or --> WebView21.CoreWebView2.Navigate(txtURL.Text)
+                WebView21.Source = New Uri(txtURL.Text)
+            Catch ex As UriFormatException
+                MessageBox.Show("The URL must be fully styled, such as the prefix. http[s]://")
+            Catch ex As Exception
+                MessageBox.Show("Error: {ex}")
+            End Try
+        End If
+    End Sub
+End Class
